0f8205bdce07ef165cb0265e50f59498be8a8998
artanton <kabletrade@gmail.com>
2024-09-26 23:22:19 +0300

    bug-fix

--- src/redux/operators.js
+++ src/redux/operators.js
@@ -1,9 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://recursive-todo-api.onrender.com/api';
+// axios.defaults.baseURL = 'https://recursive-todo-api.onrender.com/api';
 
 // axios.defaults.baseURL = 'https://668c2ba00b61b8d23b0ca4de.mockapi.io';
+// axios.defaults.baseURL = 'https://recusive-todolist-nest-mongo.onrender.com'
+axios.defaults.baseURL = 'http://localhost:4000';
 
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
@@ -21,7 +23,12 @@ export const addTask = createAsyncThunk(
   'tasks/addTask',
   async ({ text, date, parentId, subLevel }, thunkAPI) => {
     try {
-      const response = await axios.post('/tasks', { text, date, parentId, subLevel });
+      const response = await axios.post('/tasks', {
+        text,
+        date,
+        parentId,
+        subLevel,
+      });
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -45,9 +52,8 @@ export const updateTask = createAsyncThunk(
   'tasks/updateTask',
 
   async ({ taskId, text }, thunkAPI) => {
-    
     try {
-      const response = await axios.put(`/tasks/${taskId}`, { text });
+      const response = await axios.patch(`/tasks/${taskId}`, { text });
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
